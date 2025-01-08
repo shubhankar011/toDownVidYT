ccf24aa6443265df8421964a03751ea302acbeab
Shubhankar Kumar <162623205+shubhankar011@users.noreply.github.com>
2025-01-08 13:06:49 +0530

    Update main.py
    This update the whole file. The update has:
    1. Options to choose the file type whether the hd or sd
    2. Now, It can also download in audio
    3. Made a error handling feature if you get error in opening file automatically
    4. Made a feature to automatically make a folder if the user entered the wrong folder so that it won't get error
    5. Fixed some bugs

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ import os
 # Defining name and folder/path of file
 print("Try Using for e.g. : 'C://User//PC//Videos' or just 'Video'")
 dir = input("Enter the folder/path to save the file: ")
+if not os.path.exists(dir):
+    os.makedirs(dir)
 name = input("Name of the file: ")
 name = name+'.mp4'
+print("For audio-only - 'a'//0, video-only - 'v'//1, video with both in hiqh quality - 'hva', both in low quality - 'sva'")
+select = input("Enter the type of video:")
 
 #Defining Progress Hook
 def progress_hook(d):
@@ -42,20 +46,41 @@ yt_opts = {
     'logger': Logger_error(),
     'writesubtitles': True,
     'subtitleslangs': ['en'],
-    # 'postprocessors': [{ --If you've FFmpegVideoConvertor
-    #     'key':'FFmpegVideoConvertor',
-    #     'preferedformat':'mp4'
-    # }]
+    'postprocessors': [{
+        'key':'FFmpegVideoConvertor',
+        'preferedformat':'mp4'
+    }]
 
 }
 # Asking URL of a specific video, dont give url of a playlist
 urlOfVideo = input("Enter the url: ")
 
+# Making format according to the file
+if select == 'a' or select == '0':
+    yt_opts['format'] = 'bestaudio'
+
+elif select == 'v' or select == '1':
+    yt_opts['format'] = 'bestvideo'
+
+elif select=='hva' or select=='2':
+    yt_opts['format'] = 'bestaudio+bestvideo/best'
+
+elif select == 'sva' or select =='3':
+    yt_opts['format'] = 'best'
+
+with YoutubeDL(yt_opts) as ydl:
+    #Info of Video if you don't want comment the below 2 lines
+    info = ydl.extract_info(urlOfVideo, download=False)
+    # print(info)
+    file_extension = info.get('ext')
+    ydl.download(urlOfVideo)
+
 # Downloading Video
 with YoutubeDL(yt_opts) as ydl:
     #Info of Video if you don't want comment the below 2 lines
     info = ydl.extract_info(urlOfVideo, download=False)
-    print(info)
+    # print(info)
+    file_extension = info.get('ext')
     ydl.download(urlOfVideo)
 
 # If upper downloading code doesn't work then try with this
@@ -64,14 +89,19 @@ with YoutubeDL(yt_opts) as ydl:
 # print(info)
 # ydl.download(urlOfVideo)
 
+file_path = os.path.abspath(f'{dir}/{name}.{file_extension}
+
 # Playing the media from Windows Media Player
-os.startfile(f'{dir}/{name}')
+try:
+    os.startfile(file_path)
 
-# Playing the file from VLC Media Player
-player = vlc.MediaPlayer()
-file = vlc.Media(f'{dir}/{name}')
-player.set_media(file)
-
-player.play()
-while player.is_playing():
-    pass
+    # Playing the file from VLC Media Player
+    player = vlc.MediaPlayer()
+    file = vlc.Media(file_path)
+    player.set_media(file)
+    
+    player.play()
+    while player.is_playing():
+        pass
+except:
+    print(f'The file couldn't be played automatically. Go through the DIRECTORY - {file_path}')
